5b286939dc3f12ded6c5d4e37166cbdc16f8756a
ania <ania_w@outlook.com>
2014-08-28 08:44:05 +0200

    Update exercise3

---
+++ app/AnnouncementListCtrl.js
@@ -0,0 +1,10 @@
+(function () {
+    'use strict';
+    function AnnouncementListCtrl(AnnouncementDAO) {
+
+    }
+
+    var module = angular.module("exerciseApp");
+    module.controller('AnnouncementListCtrl', ['AnnouncementDAO',AnnouncementListCtrl]);
+
+})();
