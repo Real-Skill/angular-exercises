d25d74839bdb917b79e9bd61da3842cccf1d639e
ania <ania_w@outlook.com>
2014-09-05 14:10:35 +0200

    Add exercise11

---
+++ app/DragDropCtrl.js
@@ -0,0 +1,20 @@
+(function () {
+    'use strict';
+    function DragDropCtrl() {
+        var ctrl = this;
+        ctrl.container1 = [];
+        ctrl.container2 = [];
+
+        this.buttons = [
+            { 'title': 'One', 'drag': true},
+            { 'title': 'Two', 'drag': true},
+            { 'title': 'Three', 'drag': true},
+            { 'title': 'Four', 'drag': true},
+            { 'title': 'Five', 'drag': true},
+            { 'title': 'Six', 'drag': true}
+        ];
+
+    }
+    var module = angular.module('exerciseApp',[]);
+    module.controller('DragDropCtrl', [DragDropCtrl]);
+})();
