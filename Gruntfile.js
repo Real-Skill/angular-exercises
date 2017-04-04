c760479885a6ebc4743a204b2417b802de4024b3
aniaw <ania_w@outlook.com>
2017-04-04 13:38:51 +0200

    RSV adjustments

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,54 +2,91 @@
 
 module.exports = function (grunt)
 {
-  'use strict';
+    'use strict';
 
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-karma');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-connect');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-karma');
 
-  require('load-grunt-tasks')(grunt);
+    require('load-grunt-tasks')(grunt);
 
 
-  var config = {
-    app: 'app'
-  };
+    var paths = {
+        app: 'app'
+    };
 
-  grunt.initConfig({
-    config: config,
-    karma: {
-      options: {
-        configFile: 'test/karma.conf.js'
-      },
-      unit: {
-        singleRun: true
-      },
-      dev: {
-        singleRun: false
-      }
-    },
-    jshint: {
-      default: {
-        options: {
-          jshintrc: true
+    grunt.initConfig({
+        config: paths,
+        watch: {
+            livereload: {
+                options: {
+                    livereload: '<%= connect.options.livereload %>'
+                },
+                files: ['<%= paths.app %>/**/*.html', '<%= paths.app %>/**/*.js']
+            }
         },
-        files: {
-          src: ['app/**/*.js', 'test/**/*.js', '!app/bower_components/**/*.js']
+
+        connect: {
+            options: {
+                port: 9000,
+                livereload: 35729,
+                hostname: (process.env.HOSTNAME || 'localhost')
+            },
+            test: {
+                options: {
+                    base: ['app'],
+                    port: 9001
+                }
+            },
+            livereload: {
+                options: {
+                    open: true,
+                    middleware: function (connect)
+                    {
+                        return [connect().use('/bower_components', connect.static('./bower_components')), connect.static(paths.app)
+
+                        ];
+                    }
+                }
+            }
+        },
+        karma: {
+            options: {
+                configFile: 'test/karma.conf.js'
+            },
+            unit: {
+                singleRun: true
+            },
+            dev: {
+                singleRun: false
+            }
+        },
+        jshint: {
+            default: {
+                options: {
+                    jshintrc: true
+                },
+                files: {
+                    src: ['app/**/*.js', 'test/**/*.js', '!app/bower_components/**/*.js']
+                }
+            },
+            verify: {
+                options: {
+                    jshintrc: true,
+                    reporter: 'checkstyle',
+                    reporterOutput: 'target/jshint.xml'
+                },
+                files: {src: ['app/**/*.js', 'test/**/*.js', '!app/bower_components/**/*.js']}
+            }
         }
-      },
-      verify: {
-        options: {
-          jshintrc: true,
-          reporter: 'checkstyle',
-          reporterOutput: 'target/jshint.xml'
-        },
-        files: {src: ['app/**/*.js', 'test/**/*.js', '!app/bower_components/**/*.js']}
-      }
-    }
-  });
+    });
 
-  grunt.registerTask('verify', ['jshint:verify', 'karma:unit']);
+    grunt.registerTask('serve', ['connect:livereload', 'watch']);
 
-  grunt.registerTask('test:dev', ['karma:dev']);
+    grunt.registerTask('verify', ['jshint:verify', 'karma:unit']);
 
-  grunt.registerTask('default', ['verify']);
+    grunt.registerTask('test:dev', ['karma:dev']);
+
+    grunt.registerTask('default', ['serve']);
 };
