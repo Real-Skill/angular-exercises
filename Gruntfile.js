242018d489db5b2ea84ae99775381da08786ac38
Artur <rabenda27178@student.pwsztar.edu.pl>
2017-11-14 09:01:25 +0100

    add scaffolding and fix test

--- Gruntfile.js
+++ Gruntfile.js
@@ -12,43 +12,78 @@ module.exports = function (grunt)
 
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-contrib-connect');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-karma');
 
-    var config = {
+    require('load-grunt-tasks')(grunt);
+
+
+    var paths = {
         app: 'app'
     };
 
     grunt.initConfig({
-        config: config, watch: {
+        config: paths,
+        watch: {
             livereload: {
                 options: {
                     livereload: '<%= connect.options.livereload %>'
-                }, files: ['<%= config.app %>/**/*.html', '<%= config.app %>/**/*.js']
+                }, files: ['<%= paths.app %>/**/*.html', '<%= paths.app %>/**/*.js']
             }
         }, connect: {
             options: {
-                port: 9000, livereload: 35729, hostname: 'localhost'
-            }, livereload: {
+                port: 9000,
+                livereload: 35729,
+                hostname: (process.env.HOSTNAME || 'localhost')
+            }, test: {
                 options: {
-                    open: true, middleware: function (connect)
+                    base: ['app'],
+                    port: 9001
+                }
+            },
+            livereload: {
+                options: {
+                    open: true,
+                    middleware: function (connect)
                     {
-                        return [connect().use('/bower_components', connect.static('./bower_components')), connect.static(config.app)
+                        return [connect().use('/bower_components', connect.static('./bower_components')), connect.static(paths.app)
 
                         ];
                     }
                 }
             }
-        }, karma: {
-            unit: {
+        },
+        karma: {
+            options: {
                 configFile: 'test/karma.conf.js'
+            },
+            unit: {
+                singleRun: true
+            },
+            dev: {
+                singleRun: false
+            }
+        },
+        jshint: {
+            default: {
+                options: {
+                    jshintrc: true
+                }, files: {
+                    src: ['app/**/*.js', 'test/**/*.js', '!bower_components/**/*.js']
+                }
+            }, verify: {
+                options: {
+                    jshintrc: true, reporter: 'checkstyle', reporterOutput: 'target/jshint.xml'
+                }, files: {src: ['app/**/*.js', 'test/**/*.js', '!bower_components/**/*.js']}
             }
         }
     });
 
-    grunt.registerTask('serve', function ()
-    {
-        grunt.task.run(['connect:livereload', 'watch']);
-    });
+    grunt.registerTask('serve', ['connect:livereload', 'watch']);
+
+    grunt.registerTask('verify', ['jshint:verify', 'karma:unit']);
+
+    grunt.registerTask('test:dev', ['karma:dev']);
 
     grunt.registerTask('default', ['serve']);
 };
